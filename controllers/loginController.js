c3f565a71cd1331815dfae3a12bed6012f90c716
Harvin Ong <harvin.ong20@gmail.com>
2026-05-02 12:42:47 +0800

    login routes

---
+++ controllers/loginController.js
@@ -0,0 +1,7 @@
+exports.login = (req, res) => {
+    res.send("LOGIN not implemented");
+}
+
+exports.register = (req, res) => {
+    res.send("REGISTER not implemented");
+}
--- routes/base-router.js
+++ routes/base-router.js
@@ -3,4 +3,8 @@ const router = express.Router();
 
 router.get("/", (req, res) => res.send("418 I'm a Teh Poci"));
 
+// Subroutes
+const publicRouter = require('express');
+router.use("/", publicRouter);
+
 module.exports = router;
