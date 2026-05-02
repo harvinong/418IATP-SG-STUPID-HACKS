6aa5d4303f64912f922450dc299e3168ecbcc369
William Hansel <w.hansel007@gmail.com>
2026-05-02 14:36:35 +0800

    Feat: Add real posts auto

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,9 +1,64 @@
 const postScraper = require("../scrape/postScraper")
 const postModel = require("../models/postModel")
 
-exports.gameGet = (req,res) => {res.send("gameGET not implemented");};
+exports.gameGet = (req,res) => {
+
+    const placeholderPost = {
+        text: "I just had Wingstop today. So good! 🤤",
+        author: "Will B. Goodenough",
+        isReal: true,
+        link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        success: 420,
+        fail: 69
+    }
+
+    const playerStats = req.session.playerStats || {
+        points: 0
+    };
+
+    const options = {
+        post: placeholderPost,
+        playerStats,
+        outcome: 'pass'
+    };
+    res.render("game", options);
+};
+
+exports.gamePost = (req,res) => {
+    if (req.body.pass) {return res.redirect("/game/");}
+
+    if (req.session.playerStats) {
+        req.session.playerStats.points += 1;
+    } else {
+        req.session.playerStats = {points: 1};
+    }
+
+    
+    const placeholderPost = {
+        text: "I just had Wingstop today. So good! 🤤",
+        author: "Will B. Goodenough",
+        isReal: true,
+        link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+        success: 420,
+        fail: 69
+    }
+    
+    let outcome = 'fail';
+    if (req.body.real && placeholderPost.isReal) {
+        outcome = 'success';
+    }
+
+    const options = {
+        post: placeholderPost,
+        playerStats: req.session.playerStats,
+        outcome
+    };
+
+    res.render("game", options);
+};
 
 exports.addFakeGet = (req,res) => {res.send("addFakeGET not implemented");};
+
 exports.addFakePost = (req,res) => {res.send("addFakePOST not implemented");};
 
 exports.addRealGet = async (req,res) => {
@@ -20,3 +75,4 @@ exports.addRealPost = async (req,res) => {
 
    res.render("addReal")
 };
+
