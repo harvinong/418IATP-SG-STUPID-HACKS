5de54d60234cc8c70ee882131da2a72e93178d03
Harvin Ong <harvin.ong20@gmail.com>
2026-05-02 12:17:57 +0800

    Set up database connection + landing page

--- routes/base-router.js
+++ routes/base-router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
+router.get("/", (req, res) => res.send("418 I'm a Teh Poci"));
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,25 +1,56 @@
-const express = require('express');
-const session = require('express-session');
-const path = require('path');
-
+const express = require("express");
 const server = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+const session = require('express-session');
 
-const port = 8000;
-const hostname = "localhost";
+// Load Env variable
+dotenv.config({ path: './.env.local' });
+require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]);
 
-// Middleware setup
+// Middleware
+server.use(express.static(path.join(__dirname, "public")));
+server.use(express.urlencoded({ extended: true }));
 server.set('view engine', 'ejs');
-server.set('views', path.join(__dirname, 'views'));
-server.use(express.urlencoded({ extended: false }));
+server.set("views", path.join(__dirname, "views"));
 
-// Middleware for session management
+// session ID
+
+const secret = process.env.SECRET;
 server.use(session({
-    secret: 'your-secret-key',
-    resave: false,
-    saveUninitialized: false
+  secret: secret, // sign the session ID cookie. should be a long, random, and secure string, preferably stored in an environment variable
+  resave: false, // Prevents the session from being saved back to the session store if nothing has changed.
+  saveUninitialized: false, // Prevents a new, empty session from being saved to the store.
+  cookie: {
+    secure: false // even if I reload, session will stay
+  }
 }));
 
 // Routes
-server.listen(port, hostname,() => {
+const baseRouter = require("./routes/base-router.js");
+server.use("/", baseRouter);
+
+// Database connection
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.DB_MONGODB_URI);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1);
+  }
+};
+
+// Initialize Server
+function startServer() {
+  const hostname = "localhost";
+  const port = 8000;
+
+  server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
-});
+  });
+}
+
+// call connectDB first and when connection is ready we start the web server
+connectDB().then(startServer);
