9db0f6ea761ffb965b51461a3ef035bb17128104
Harvin Ong <harvin.ong20@gmail.com>
2026-05-02 13:41:22 +0800

    feat: 404 page

--- routes/base-router.js
+++ routes/base-router.js
@@ -9,4 +9,6 @@ const gameRouter = require('./game-routes');
 router.use("/", publicRouter);
 router.use("/game/", gameRouter);
 
+router.all(/_*/g, (req, res) => res.status(404).send("<h1>404 Not found</h1><p>This message is crafted with love by <strong>418 I'm a Teh Poci</strong> 🥰🥰🥰😻</p>"));
+
 module.exports = router;
