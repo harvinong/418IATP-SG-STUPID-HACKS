e168d6a449dcd6a6477fb1b8252c2a1fa999e9c1
Harvin Ong <harvin.ong20@gmail.com>
2026-05-02 13:21:39 +0800

    feat: add game routes

---
+++ controllers/gameControllers.js
@@ -0,0 +1,3 @@
+exports.gameGet = (req,res) => {res.send("gameGET not implemented");};
+exports.addFakeGet = (req,res) => {res.send("addFakeGET not implemented");};
+exports.addFakePost = (req,res) => {res.send("addFakePOST not implemented");};
--- routes/game-routes.js
+++ routes/game-routes.js
@@ -1,2 +1,10 @@
 const express = require("express");
 const router = express.Router();
+
+// Route
+const gameControllers = require("../controllers/gameControllers");
+router.get("/", gameControllers.gameGet);
+router.get("/add-fake", gameControllers.addFakeGet);
+router.post("/add-fake", gameControllers.addFakePost);
+
+module.exports = router;
