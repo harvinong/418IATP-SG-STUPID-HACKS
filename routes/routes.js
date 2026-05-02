dfc86f32ea4fc50af837eb6ce4224968d948d434
William Hansel <w.hansel007@gmail.com>
2026-05-02 11:33:45 +0800

    Chore: Files Cleanup

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const peopleController = require('../controllers/controllers');
 
-// Route to display the main input form and the list of entered people
-router.get('/', peopleController.homeGet );
-
-// Route to process the form submission
-router.post('/' , peopleController.homePost);
 
 module.exports = router;
