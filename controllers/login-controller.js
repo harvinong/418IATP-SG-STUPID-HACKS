1da969e6c25ed51708e6dbda0dc68013fb7d79a1
Harvin Ong <harvin.ong20@gmail.com>
2026-05-02 16:07:42 +0800

    login routes

--- controllers/login-controller.js
+++ controllers/login-controller.js
@@ -1,7 +1,15 @@
-exports.login = (req, res) => {
+exports.loginGet = (req, res) => {
     res.render("login", { error: null });
 };
 
-exports.register = (req, res) => {
+exports.loginPost = (req, res) => {
+    res.send("loginPost not implemented");
+};
+
+exports.registerGet = (req, res) => {
     res.render("register");
+};
+
+exports.registerPost = (req, res) => {
+    res.send("registerPost not implemented");
 };
--- routes/game-routes.js
+++ routes/game-routes.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const authMiddleware = require("../middleware/authMiddleware");
 
 // Route
 const gameController = require("../controllers/game-controller");
 router.get("/", gameController.gameGet);
 router.post("/", gameController.gamePost);
+
+// router.get("/add-fake", authMiddleware.isLoggedIn, gameController.addFakeGet);
+// router.post("/add-fake", authMiddleware.isLoggedIn, gameController.addFakePost);
+
+// router.get("/add-real", authMiddleware.isLoggedIn, gameController.addRealGet)
+// router.post("/add-real", authMiddleware.isLoggedIn, gameController.addRealPost)
+
+// router.get("/saw-real", authMiddleware.isLoggedIn, gameController.addSawRealGet)
+// router.post("/saw-real", authMiddleware.isLoggedIn, gameController.addSawRealPost)
 
 router.get("/add-fake", gameController.addFakeGet);
 router.post("/add-fake", gameController.addFakePost);
